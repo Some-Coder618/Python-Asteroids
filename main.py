311f497129ca4d5743fe465063293430baf95dc6
Some-Coder618 <macsen.maglinao@gmail.com>
2025-10-24 16:08:38 -0600

    2: limited framerate to 60 and added delta time variable

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("Black")
         pygame.display.flip()
+        dt = clock.tick(60)
+        
 if __name__ == "__main__":
     main()
