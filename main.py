6b079e4d3cf33d275b59520c8f3a396444ca9724
Some-Coder618 <macsen.maglinao@gmail.com>
2025-10-25 15:30:47 -0600

    7: added containers and put player in 'drawable' and 'updatable' containers, also updated movement speed to 200

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from player import *
 
 
 def main():
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     clock = pygame.time.Clock()
     dt = 0
 
@@ -19,8 +25,9 @@ def main():
                 return
         dt = clock.tick(60) / 1000
         screen.fill("Black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
 
         
