123346a67d1bdf879be4553e3b76072f53bc7074
Some-Coder618 <macsen.maglinao@gmail.com>
2025-10-25 14:54:46 -0600

    5: added player roation

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
         screen.fill("Black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+
+        
         
 if __name__ == "__main__":
     main()
