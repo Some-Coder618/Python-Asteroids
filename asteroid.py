1aecdafa5e68fda9f6216017b6f141c9463544d8
Some-Coder618 <macsen.maglinao@gmail.com>
2025-10-26 01:13:08 -0600

    17: added 'non-circular' asteroids to asteroids.py

--- asteroid.py
+++ asteroid.py
@@ -5,10 +5,22 @@ import random
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+    def pentagon(self):
+        forward = pygame.Vector2(0, 1)
+        right = pygame.Vector2(0, 1).rotate(90) * self.radius / 1.5
+        a = self.position - forward * self.radius
+        b = self.position - forward * 0.05 * self.radius - right
+        c = self.position + forward * self.radius - right * 0.5
+        d = self.position + forward * self.radius + right * 0.5
+        e = self.position - forward * 0.05 * self.radius + right
+        return [a,b,c,d,e]
 
+
+    
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
-
+        #pygame.draw.polygon(screen, "white", self.pentagon() , 2)
+        # optional circular asteroids
+         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
     def update(self, dt):
         self.position += self.velocity * dt
 
--- player.py
+++ player.py
@@ -13,8 +13,8 @@ class Player(CircleShape):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
         a = self.position + forward * self.radius
-        b = self.position - forward * self.radius - right
-        c = self.position - forward * self.radius + right
+        b = self.position - forward * self.radius + right
+        c = self.position - forward * self.radius - right
         return [a, b, c]
     
     def draw(self, screen):
